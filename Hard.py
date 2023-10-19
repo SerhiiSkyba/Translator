ccac7dbaeb3a4434af4b6da9b08895c341a494fb
SerhiiSkyba <128896418+SerhiiSkyba@users.noreply.github.com>
2023-10-19 16:23:32 +0200

    OK

--- Hard.py
+++ Hard.py
@@ -1,60 +1,116 @@
 import customtkinter as ctk
 from deep_translator import GoogleTranslator
 import random
+from tkinter import messagebox
 from Baza import listnum, listword
 
+# FUNKCJA SPRAWDZAJĄCA
+def funkcja_sprawdzenia():
+    global zycia, score, ilosc_pytan_odpowiedzialnych, wylosowane_slowo
+    
+    
+    # POPRAWNA ODPOWIEDZ
+    if prompt.get() == GoogleTranslator(source='pl', target='en').translate(wylosowane_slowo):
+        try:
+            score = score + 1
 
-def pri():
-    global y
-    global z
-    if entryv.get() == GoogleTranslator(source='pl', target='en').translate(y):
-        y = random.choice(list2)
-        labelv.set(y)
-        labelv2.set(GoogleTranslator(source='pl', target='en').translate(y))
-        z=z+1
-        labelv3.set(str(z)+"/20")
+            wylosowane_slowo = random.choice(pytania)
+
+            pytania.remove(wylosowane_slowo)
+
+            wylosowane_slowo_pl.set(wylosowane_slowo)
+
+            wylosowane_slowo_en.set(GoogleTranslator(source='pl', target='en').translate(wylosowane_slowo))
+
+            ilosc_pytan_odpowiedzialnych = ilosc_pytan_odpowiedzialnych+1
+
+            prompt.get('')
+
+            ilosc_pytan.set(str(ilosc_pytan_odpowiedzialnych)+"/20")
+        except:
+            messagebox.showinfo(title='Wygrałeś',message='Ty wygrałeś, twój wynnik jest '+str(score)+'/20')
+            windowgra.destroy()
+    
+    # ŻLA ODPOWIEDZ
     else:
-        y = random.choice(list2)
-        labelv.set(y)
-        labelv2.set(GoogleTranslator(source='pl', target='en').translate(y))
-        z=z+1
-        labelv3.set(str(z)+"/20")
-        labelv4.set(labelv4.get()-1)
+        try:
+            wylosowane_slowo = random.choice(pytania)
+
+            pytania.remove(wylosowane_slowo)
+
+            wylosowane_slowo_pl.set(wylosowane_slowo)
+
+            wylosowane_slowo_en.set(GoogleTranslator(source='pl', target='en').translate(wylosowane_slowo))
+
+            ilosc_pytan_odpowiedzialnych = ilosc_pytan_odpowiedzialnych+1
+
+            ilosc_pytan.set('Pytanie'+str(ilosc_pytan_odpowiedzialnych)+"/20")
+
+            zycia = zycia - 1
+            ilosc_zyc.set('Sprób zostało się'+str(zycia))
+
+            if zycia == 0 or zycia < 0 :
+                messagebox.showinfo(title='Przegrałeś',message='Ty przegrałeś, twój wynnik jest '+str(score))
+                windowgra.destroy()
+        except:
+            messagebox.showinfo(title='Wygrałeś',message='Ty wygrałeś, twój wynnik jest '+str(score)+'/20')
+            windowgra.destroy()
+
 windowgra = ctk.CTk()
 
-list2 = []
+# WYLOSOWANIE PYTAŃ
+pytania = []
 for i in range (1,20):
     x = random.choice(listword)
-    list2.append(x)
+    pytania.append(x)
     listword.remove(x)
 
-print (list2)
+# print (pytania) Wypisuje pytania wylosowane z bazy
 
-labelv = ctk.StringVar()
-entryv = ctk.StringVar()
-labelv2 = ctk.StringVar()
-labelv3 = ctk.StringVar()
-labelv4 = ctk.IntVar()
+# ZMIENNE GRAFICZNE
+wylosowane_slowo_pl = ctk.StringVar()
+prompt = ctk.StringVar()
+wylosowane_slowo_en = ctk.StringVar()
+ilosc_pytan = ctk.StringVar()
+ilosc_zyc = ctk.StringVar()
 
-z = 0
-labelv3.set(str(z)+"/20")
-labelv4.set(4)
-y = random.choice(list2)
-labelv.set(y)
-labelv2.set(GoogleTranslator(source='pl', target='en').translate(y))
+# STARTOWE VALUES ZMIENNYCH
+score = 0
+zycia = 4
+ilosc_pytan_odpowiedzialnych = 1
+ilosc_pytan.set('Pytanie '+str(ilosc_pytan_odpowiedzialnych)+"/20")
+ilosc_zyc.set('Sprób zostało się '+str(zycia))
+wylosowane_slowo = random.choice(pytania)
+wylosowane_slowo_pl.set(wylosowane_slowo)
+pytania.remove(wylosowane_slowo)
+wylosowane_slowo_en.set(GoogleTranslator(source='pl', target='en').translate(wylosowane_slowo))
 
-label = ctk.CTkLabel(windowgra, textvariable = labelv)
-label2 = ctk.CTkLabel(windowgra, textvariable = labelv2)
-label3 = ctk.CTkLabel(windowgra, textvariable = labelv3)
-label4 = ctk.CTkLabel(windowgra, textvariable = labelv4)
-button = ctk.CTkButton(windowgra, text = 'Zatwierdz', command=lambda:pri())
-entry = ctk.CTkEntry(windowgra, textvariable=entryv)
+# CZĘŚĆ GRAFICZNA
+slowo_pl = ctk.CTkLabel(windowgra,
+                        textvariable = wylosowane_slowo_pl)
 
-label.pack()
-label2.pack()
-label3.pack()
-label4.pack()
-entry.pack()
-button.pack()
+slowo_en = ctk.CTkLabel(windowgra,
+                        textvariable = wylosowane_slowo_en)
+
+questions = ctk.CTkLabel(windowgra,
+                         textvariable = ilosc_pytan)
+
+lives = ctk.CTkLabel(windowgra,
+                     textvariable = ilosc_zyc)
+
+confirm_btn = ctk.CTkButton(windowgra,
+                            text = 'Zatwierdz',
+                            command=lambda:funkcja_sprawdzenia())
+
+pole_wpisu = ctk.CTkEntry(windowgra,
+                          textvariable=prompt)
+
+# CZĘŚĆ IMPORTACJI GRAFIKI
+slowo_pl.pack()
+slowo_en.pack()
+questions.pack()
+lives.pack()
+pole_wpisu.pack()
+confirm_btn.pack()
 
 windowgra.mainloop()
--- NormalHard.py
+++ NormalHard.py
@@ -1,3 +1,61 @@
 import customtkinter as ctk
+from deep_translator import GoogleTranslator
+import random
+from tkinter import messagebox
+from Baza import listnum, listword
 windowgra = ctk.CTk()
+# WYLOSOWANIE PYTAŃ
+pytania = []
+for i in range (1,20):
+    x = random.choice(listword)
+    pytania.append(x)
+    listword.remove(x)
+
+# print (pytania) Wypisuje pytania wylosowane z bazy
+
+# ZMIENNE GRAFICZNE
+wylosowane_slowo_pl = ctk.StringVar()
+prompt = ctk.StringVar()
+wylosowane_slowo_en = ctk.StringVar()
+ilosc_pytan = ctk.StringVar()
+ilosc_zyc = ctk.StringVar()
+
+# STARTOWE VALUES ZMIENNYCH
+score = 0
+zycia = 4
+ilosc_pytan_odpowiedzialnych = 1
+ilosc_pytan.set('Pytanie '+str(ilosc_pytan_odpowiedzialnych)+"/20")
+ilosc_zyc.set('Sprób zostało się '+str(zycia))
+wylosowane_slowo = random.choice(pytania)
+wylosowane_slowo_pl.set(wylosowane_slowo)
+pytania.remove(wylosowane_slowo)
+wylosowane_slowo_en.set(GoogleTranslator(source='pl', target='en').translate(wylosowane_slowo))
+
+# CZĘŚĆ GRAFICZNA
+slowo_pl = ctk.CTkLabel(windowgra,
+                        textvariable = wylosowane_slowo_pl)
+
+slowo_en = ctk.CTkLabel(windowgra,
+                        textvariable = wylosowane_slowo_en)
+
+questions = ctk.CTkLabel(windowgra,
+                         textvariable = ilosc_pytan)
+
+lives = ctk.CTkLabel(windowgra,
+                     textvariable = ilosc_zyc)
+
+confirm_btn = ctk.CTkButton(windowgra,
+                            text = 'Zatwierdz',
+                            command=lambda:funkcja_sprawdzenia())
+
+pole_wpisu = ctk.CTkEntry(windowgra,
+                          textvariable=prompt)
+
+# CZĘŚĆ IMPORTACJI GRAFIKI
+slowo_pl.pack()
+slowo_en.pack()
+questions.pack()
+lives.pack()
+pole_wpisu.pack()
+confirm_btn.pack()
 windowgra.mainloop()
