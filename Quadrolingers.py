25e6143b8d6b442c389d1b7161ada14291b4a1e3
SerhiiSkyba <128896418+SerhiiSkyba@users.noreply.github.com>
2023-10-25 10:58:13 +0200

    Rele

--- Quadrolingers.py
+++ Quadrolingers.py
@@ -1,29 +1,48 @@
 import os
 import sys
+import random
 if sys.argv:
     filepath = sys.argv[0]
     folder, filename = os.path.split(filepath)
     os.chdir(folder) # now your working dir is the parent folder of the script
 import customtkinter as ctk
-
+from tkinter import *
 def start():
+    window.destroy()
     from Easy import windowgra
 def start2():
+    window.destroy()
     from Normal import windowgra
 def start3():
+    window.destroy()
     from NormalHard import windowgra
 def start4():
+    window.destroy()
     from Hard import windowgra
+def exit():
+    window.destroy()
 window = ctk.CTk()
+bg = PhotoImage(file = "tlo.png")
+window.resizable(False,False)
+window.geometry("600x330")
+window.title("Quatrolingers")
+a = random.randint(1,4) 
+if (a!=1):
+    window.iconbitmap("image.ico")
+else:
+    window.iconbitmap("Heinz_Doofenshmirtz.ico")
 
-label = ctk.CTkLabel(window, text = 'Quadrolingers')
+
+label = ctk.CTkLabel(window,image = bg, text="")
 button1 = ctk.CTkButton(window, text = 'Poziom łatwy', command = lambda:start())
 button2 = ctk.CTkButton(window, text = 'Poziom normalny', command = lambda:start2())
 button3 = ctk.CTkButton(window, text = 'Poziom normalno-trudny', command = lambda:start3())
 button4 = ctk.CTkButton(window, text = 'Poziom trudny', command = lambda:start4())
+button5 = ctk.CTkButton(window,text="🚪",font=("Calibri",30),command=lambda:exit())
 label.pack()
-button1.pack()
-button2.pack()
-button3.pack()
-button4.pack()
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack(side=LEFT)
+button5.pack(side=LEFT)
 window.mainloop()
